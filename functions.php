be61665401257877453e590f93ac8e7816f3a55d
Adrian Alwishewa <adrian@marlincommunications.com>
2023-11-29 16:15:38 +0530

    overriding the social media template for the footer. Adding footer styles for custom footer

--- functions.php
+++ functions.php
@@ -11,21 +11,6 @@ function enqueue_child_styles() {
 
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [], $css_creation );
 }
-
 if( function_exists('acf_add_options_page') ) {
-
-    acf_add_options_page(array(
-        'page_title'    => 'Theme General Settings',
-        'menu_title'    => 'Theme Settings',
-        'menu_slug'     => 'theme-general-settings',
-        'capability'    => 'edit_posts',
-        'redirect'      => false
-    ));
-    
-    acf_add_options_sub_page(array(
-        'page_title'    => 'Theme Footer Settings',
-        'menu_title'    => 'Footer',
-        'parent_slug'   => 'theme-general-settings',
-    ));
-
+    acf_add_options_page();
 }
