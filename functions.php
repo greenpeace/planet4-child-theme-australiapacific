5a1889c1f33c6918391cfa76c137e7ed557fc49e
Adrian Alwishewa <adrian@marlincommunications.com>
2023-12-07 13:32:22 +0530

    completing rebase

--- functions.php
+++ functions.php
@@ -19,37 +19,3 @@ function register_custom_script() {
   wp_enqueue_script( 'marlin-script', get_stylesheet_directory_uri() .  '/js/scripts.js' );
 }
 add_action( 'wp_enqueue_scripts', 'register_custom_script' );
-
-// SVG Support
-add_filter( 'wp_check_filetype_and_ext', function($data, $file, $filename, $mimes) {
-  global $wp_version;
-  if ( $wp_version !== '4.7.1' ) {
-     return $data;
-  }
-  $filetype = wp_check_filetype( $filename, $mimes );
-  return [
-      'ext'             => $filetype['ext'],
-      'type'            => $filetype['type'],
-      'proper_filename' => $data['proper_filename']
-  ];
-}, 10, 4 );
-
-function cc_mime_types( $mimes ){
-  $mimes['svg'] = 'image/svg+xml';
-  return $mimes;
-}
-add_filter( 'upload_mimes', 'cc_mime_types' );
-
-function fix_svg() {
-  echo '<style type="text/css">
-        .attachment-266x266, .thumbnail img {
-             width: 100% !important;
-             height: auto !important;
-        }
-        </style>';
-}
-<<<<<<< HEAD
-add_action( 'admin_head', 'fix_svg' );
-=======
-add_action( 'admin_head', 'fix_svg' );
->>>>>>> 5664b2f (adding SVG support and updating footer styles and bg)
