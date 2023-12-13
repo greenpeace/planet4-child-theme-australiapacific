3eae2bc3df6dd00dfcc8713768172df2cba1204c
kbgreenpeace <107445567+kbgreenpeace@users.noreply.github.com>
2023-12-13 20:01:12 +1100

    Update functions.php

--- functions.php
+++ functions.php
@@ -49,33 +49,33 @@ add_action( 'admin_head', 'fix_svg' );
 if (function_exists('acf_add_local_field_group')) {
 
   acf_add_local_field_group(array(
-      'key' => 'group_6579185b4dab2',
+      'key' => 'group_65540d4060e4e',
       'title' => 'Author Profile',
       'fields' => array(
 	        // Add Long Bio Field
           array(
-              'key' => 'field_6579501f61353',
+              'key' => 'field_65540d4fbe46e',
               'label' => 'Long Bio',
               'name' => 'long_bio',
               'type' => 'wysiwyg',
           ),
           // Add a Role field
           array(
-              'key' => 'field_657957dc3cd62',
+              'key' => 'field_65541c2c2230a',
               'label' => 'Role',
               'name' => 'role',
               'type' => 'text',
           ),
           // Add a Bio Image field
           array(
-              'key' => 'field_657957c03cd61',
+              'key' => 'field_657904407246a',
               'label' => 'Bio Image',
               'name' => 'bio_image',
               'type' => 'image',
           ),
           // Add a LinkedIn Profile field
           array(
-             'key' => 'field_657958313cd63',
+             'key' => 'field_65541d8c0b0a7',
               'label' => 'Linkedin Profile',
               'name' => 'linkedin_profile',
               'type' => 'url',
