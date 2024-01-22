8e752c3e099469f7e7607db7316969d20d2c54da
Adrian Alwishewa <adrian@marlincommunications.com>
2024-01-22 15:14:58 +1100

    updating the custom script function to only load on the homepage

--- functions.php
+++ functions.php
@@ -16,8 +16,8 @@ if( function_exists('acf_add_options_page') ) {
 }
 
 function register_custom_script() {
-  wp_enqueue_script( 'logo-resize-script', get_stylesheet_directory_uri() .  '/js/logo-resize-script.js' );
+  if(is_front_page()){
+    wp_enqueue_script( 'logo-resize-script', get_stylesheet_directory_uri() .  '/js/logo-resize-script.js' );
+  }
 }
-if(is_front_page()){
-  add_action( 'wp_enqueue_scripts', 'register_custom_script' );
-}
+add_action( 'wp_enqueue_scripts', 'register_custom_script' );
