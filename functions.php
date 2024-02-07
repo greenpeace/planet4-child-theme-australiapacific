269d0b5070b2dca9d69a883298010aa11f2950a5
Peter Weltë <techieshark+peter.welte.at.greenpeace.org@gmail.com>
2024-02-08 08:38:47 +1100

    feat(navigation-bar): ✨ homepage navbar style changes on-scroll
    What:
    * Initially: a) the logo will be bigger, and b) the nav bar is transparent
      but when user scrolls down, 1) the logo shrinks and 2) the nav becomes
      white again (like a typical P4 site).
    
    How:
    * add javascript code to add 'scrolled' class on document.body
    * added header navigation SCSS styles which update colors and logo size
      based on whether body.scrolled or :not(.scrolled)
    * modified functions.php so we load the JS file on the home page

--- functions.php
+++ functions.php
@@ -8,6 +8,13 @@ add_action('wp_enqueue_scripts', 'enqueue_child_styles', 99);
 
 function enqueue_child_styles() {
     $css_creation = filectime(get_stylesheet_directory() . '/style.css');
-
-    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [], $css_creation );
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', [], $css_creation);
 }
+
+function register_custom_script() {
+    if (is_front_page()) {
+        wp_enqueue_script('update-DOM-on-scroll', get_stylesheet_directory_uri() .  '/js/update-DOM-on-scroll.js');
+    }
+}
+
+add_action('wp_enqueue_scripts', 'register_custom_script');
