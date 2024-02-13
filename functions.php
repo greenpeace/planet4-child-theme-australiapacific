00571f097551d88b2e7b3f65d21952a142c45f70
Kara Bishop <107445567+karabishopgreenpeace@users.noreply.github.com>
2024-02-13 16:52:57 +1100

    Custom Post Type Templates (#40)
    * Create single.twig
    
    Duplicate single.twig from Master Theme
    
    * Add Template Part theme support
    
    Update functions.php to add Template parts theme support
    
    * Add Placeholder HTML Files
    
    These files will be referenced in the Post Type template files. They are just placeholder content which can be edited in Gutenberg.
    
    * Include Template Parts to Post Types
    
    To include custom blocks on News Articles. We need to add a new custom post type (single-news.twig). Which is referenced with an 'if' statement in single.twig. Also including an Archive Block if the post is tagged with 'Archive'.
    
    * Duplicate Single.twig files
    
    Make a duplicate of the single.twig file to create post type templates. No changes have been made to the duplicates yet.
    
    * Create Post Type Templates for all Post Types
    
    Remove all unused elements such as author-block and remove css classes for full width.
    Create 2 additional HTML footer files for Green guardian templates.
    
    * Add place descriptions to HTML Files
    
    Adding a comment mentioning where the block is used.
    
    * Remove CTA Template Parts
    
    These files are no-longer needed.
    
    I will add the CTA Block as an action block based on tags in a future feature request.
    
    * Fixed indentation
    
    * Remove Comments
    
    * feat: :art: Indentation in HTML
    
    Why?
    - Just making consistent with GPI's master theme
    
    ---------
    
    Co-authored-by: kbgreenpeace <107445567+kbgreenpeace@users.noreply.github.com>
    Co-authored-by: Shoryu Das-Zaman <58425525+ShoryuDZ@users.noreply.github.com>

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 /**
  * Additional code for the child theme goes in here.
+ * TODO: Document each function and action within this code.
  */
 
 add_action('wp_enqueue_scripts', 'enqueue_child_styles', 99);
@@ -15,6 +16,30 @@ function register_custom_script() {
     if (is_front_page()) {
         wp_enqueue_script('update-DOM-on-scroll', get_stylesheet_directory_uri() .  '/js/update-DOM-on-scroll.js');
     }
+}
+
+function fix_svg() {
+  echo '<style type="text/css">
+        .attachment-266x266, .thumbnail img {
+             width: 100% !important;
+             height: auto !important;
+        }
+        </style>';
+}
+
+add_action( 'admin_head', 'fix_svg' );
+
+// Template parts theme support
+function add_block_template_part_support() {
+  add_theme_support( 'block-template-parts' );
+}
+
+add_action( 'after_setup_theme', 'add_block_template_part_support' );
+
+function get_template_content($name = null){
+  ob_start();
+  block_template_part($name);
+  return ob_get_clean();
 }
 
 add_action('wp_enqueue_scripts', 'register_custom_script');
