b20a6aeb2098d64a5eac2ddfc5b75f5a81500e34
Mark Davies <mark@markdavies.com.au>
2026-04-28 21:13:57 +1000

    minify child styles

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
-module.exports = {
+module.exports = (ctx) => ({
   plugins: [
     require('@csstools/postcss-sass')(),
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
+    ctx.env === 'production' && require('cssnano')({ preset: 'default' }),
   ],
-};
+});
